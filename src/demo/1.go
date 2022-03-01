91dcf2d07592c12288cdd3669a1c14e23cf30233
Passion-Logan <wangdadamailbox@163.com>
2022-03-01 18:01:02 +0800

    update

--- src/demo/1.go
+++ src/demo/1.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+/**
+Golang数字排列组合:
+有 1、2、3、4 这四个数字，能组成多少个互不相同且无重复数字的三位数？都是多少？
+*/
+func main() {
+	totalCount := 0
+
+	for i := 1; i < 5; i++ {
+		for j := 1; j < 5; j++ {
+			for k := 1; k < 5; k++ {
+				if i != k && i != j && j != k {
+					totalCount++
+					fmt.Println("第", totalCount, "方案", i, j, k)
+				}
+
+			}
+		}
+	}
 }
